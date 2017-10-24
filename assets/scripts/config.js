ef36224e43ce5c91605a9ad561777a125deb52b1
mgnieman <michael.nieman@libertymutual.com>
2017-10-24 14:25:09 -0400

    Change production heroku link
    Switched from ga-boston to mgnieman

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://ridetrackermn.herokuapp.com/'
   }
 }
 
